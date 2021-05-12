d4799965f64909e265017eb8c69bf50d34507e35
xiaoming <xiaoming@qq.com>
2021-05-12 14:07:43 +0800

    张三第一次提交代码

--- 01-git代码的测试/src/com/gec/aa/DemoTest1.java
+++ 01-git代码的测试/src/com/gec/aa/DemoTest1.java
@@ -2,6 +2,9 @@ package com.gec.aa;
 
 public class DemoTest1 {
 
+    public static void   aaa(){
+        System.out.println("111111111");
+    }
     public static void main(String[] args) {
         System.out.println("111111111");
         System.out.println("111111111");
